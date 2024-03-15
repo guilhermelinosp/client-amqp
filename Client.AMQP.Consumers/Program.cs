5bf2a8da6596d246cc2789c73bf344fb0b986f2a
Guilherme Lino <guilhermelinosp@gmail.com>
2024-03-15 07:40:47 -0300

    feat: add project in repo

---
+++ Client.AMQP.Consumers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+const string exchangeName = "exchange.class";
+const string queueName = "queue.class";
+
+try
+{
+	var factory = new ConnectionFactory
+	{
+		HostName = "localhost",
+		UserName = "guest",
+		Password = "guest"
+	};
+
+	using var connection = factory.CreateConnection();
+	using var channel = connection.CreateModel();
+
+	channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Direct, durable: true, autoDelete: false, arguments: null);
+	channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+	channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "");
+
+	var consumer = new EventingBasicConsumer(channel);
+	consumer.Received += (_, ea) =>
+	{
+		var body = ea.Body.ToArray();
+		var message = Encoding.UTF8.GetString(body);
+		Console.WriteLine("Received message: " + message);
+	};
+
+	channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+	Console.WriteLine("Consumer started. Press [Enter] to exit.");
+	Console.ReadLine();
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"An error occurred: {ex.Message}");
+}
---
+++ Client.AMQP.Producers/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+const string exchangeName = "exchange.class";
+const string queueName = "queue.class";
+
+var stopRequested = false;
+
+try
+{
+	var factory = new ConnectionFactory
+	{
+		HostName = "localhost",
+		UserName = "guest",
+		Password = "guest"
+	};
+
+	using var connection = factory.CreateConnection();
+	using var channel = connection.CreateModel();
+
+	channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Direct, durable: true, autoDelete: false,
+		arguments: null);
+
+	channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+	Console.WriteLine("Producer started. Press [Enter] to stop.");
+
+	var inputThread = new Thread(() =>
+	{
+		while (true)
+		{
+			if (Console.ReadKey(intercept: true).Key != ConsoleKey.Enter) continue;
+			stopRequested = true;
+			break;
+		}
+	});
+	
+	inputThread.Start();
+
+	while (!stopRequested)
+	{
+		var message = new Message
+		{
+			Name = GetRandomName(),
+			Age = GetRandomAge()
+		};
+
+		channel.BasicPublish(
+			exchange: exchangeName,
+			routingKey: "",
+			basicProperties: null,
+			body: Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message))
+		);
+
+		Console.WriteLine("Message published: " + message.Id);
+	}
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"An error occurred: {ex.Message}");
+}
+
+static string GetRandomName()
+{
+	string[] names = ["John", "Alice", "Bob", "Eve", "Michael", "Emily", "David", "Sarah", "Daniel", "Olivia"];
+	var random = new Random();
+	return names[random.Next(names.Length)];
+}
+
+static int GetRandomAge()
+{
+	var random = new Random();
+	return random.Next(18, 60);
+}
+
+record Message
+{
+	public Guid Id { get; private set; } = Guid.NewGuid();
+	public required string Name { get; set; }
+	public required int Age { get; set; }
+	public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+}
